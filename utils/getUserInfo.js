41f37f3ab08ac3b6d98e18f7e1d868440a18e1f4
Muna <mmhussein125@gmail.com>
2018-12-26 20:17:20 +0000

    Adds getUserInfo function which uses Expo Constants

---
+++ utils/getUserInfo.js
@@ -0,0 +1,11 @@
+import { Constants } from 'expo';
+
+function getUserInfo() {
+  const { deviceId, deviceName, platform } = Constants;
+  return {
+      deviceId,
+      deviceName,
+      platform,
+  };
+}
+export default getUserInfo
