970d348d7e47ac03a59ec562ed0e05808ae12095
Muna <mmhussein125@gmail.com>
2018-12-26 19:58:06 +0000

    Uploading data - passing a local image URI & text to the post method

--- Fire.js
+++ Fire.js
@@ -62,6 +62,33 @@ class Fire {
       alert(message);
     }
   };
+
+  // Upload Data
+  uploadPhotoAsync = async uri => {
+    const path = `${collectionName}/${this.uid}/${uuid.v4()}.jpg`;
+    return uploadPhoto(uri, path);
+  };
+
+  post = async ({ text, image: localUri }) => {
+    try {
+      const { uri: reducedImage, width, height } = await shrinkImageAsync(
+        localUri,
+      );
+
+      const remoteUri = await this.uploadPhotoAsync(reducedImage);
+      this.collection.add({
+        text,
+        uid: this.uid,
+        timestamp: this.timestamp,
+        imageWidth: width,
+        imageHeight: height,
+        image: remoteUri,
+        user: getUserInfo(),
+      });
+    } catch ({ message }) {
+      alert(message);
+    }
+  };
 }
 
 Fire.shared = new Fire();
