7789e2058abc8b77fdb303a5d2da42873d4eade7
Muna <mmhussein125@gmail.com>
2018-12-26 19:56:18 +0000

    Uses getPaged function to download data using a pagination method

--- Fire.js
+++ Fire.js
@@ -28,6 +28,40 @@ class Fire {
       }
     });
   }
+
+  // Download Data
+  getPaged = async ({ size, start }) => {
+    let ref = this.collection.orderBy('timestamp', 'desc').limit(size);
+    try {
+      if (start) {
+        ref = ref.startAfter(start);
+      }
+
+      const querySnapshot = await ref.get();
+      const data = [];
+      querySnapshot.forEach(function(doc) {
+        if (doc.exists) {
+          const post = doc.data() || {};
+
+          // Reduce the name
+          const user = post.user || {};
+
+          const name = user.deviceName;
+          const reduced = {
+            key: doc.id,
+            name: (name || 'Secret Duck').trim(),
+            ...post,
+          };
+          data.push(reduced);
+        }
+      });
+
+      const lastVisible = querySnapshot.docs[querySnapshot.docs.length - 1];
+      return { data, cursor: lastVisible };
+    } catch ({ message }) {
+      alert(message);
+    }
+  };
 }
 
 Fire.shared = new Fire();
