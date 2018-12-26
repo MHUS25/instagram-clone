3fcb20e29b7d9753f207598509835d3080259316
Muna <mmhussein125@gmail.com>
2018-12-26 19:48:27 +0000

    Creates main tab navigator

--- App.js
+++ App.js
@@ -1,1 +1,35 @@
 import { createBottomTabNavigator, createStackNavigator, } from 'react-navigation';
+import tabBarIcon from './utils/tabBarIcon';
+import FeedScreen from './screens/FeedScreen';
+import NewPostScreen from './screens/NewPostScreen';
+import SelectPhotoScreen from './screens/SelectPhotoScreen';
+
+// Create main tab navigator for moving between the Feed and Photo screens
+const navigator = createBottomTabNavigator(
+  {
+    // The name `Feed` is used later for accessing screens
+    Feed: {
+      // Define the component we will use for the Feed screen.
+      screen: FeedScreen,
+      navigationOptions: {
+        // Add a Material Icon for this screen
+        tabBarIcon: tabBarIcon('home'),
+      },
+    },
+    // And for the Photo screen
+    Photo: {
+      screen: SelectPhotoScreen,
+      navigationOptions: {
+        tabBarIcon: tabBarIcon('add-circle'),
+      },
+    },
+  },
+  {
+    // Hiding the labels and setting a 2-tone tint system for our tabs
+    tabBarOptions: {
+      showLabel: false,
+      activeTintColor: 'black',
+      inactiveTintColor: 'gray',
+    },
+  },
+);
